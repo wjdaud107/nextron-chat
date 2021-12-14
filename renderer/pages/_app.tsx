5ee000e4e40784b316f81bfdc07c5bada1e1422f
LimJungMyeong <freewjdaud@gmail.com>
2021-12-14 22:21:22 +0900

    css 파일 명칭 수정

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ContextProvider } from '../context';
-import '../styles/auth.css';
+import '../styles/home.css';
 import '../styles/chats.css';
 import '../styles/index.css';
 
